d619ee0333bd375a933ffc24c9a013aedd1c2137
Maina <alex.maina@savannahinformatics.com>
2023-03-22 20:57:10 +0300

    clean up

--- mpesa_api/settings.py
+++ mpesa_api/settings.py
@@ -6,20 +6,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SIRI_YANGU')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','*']
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
